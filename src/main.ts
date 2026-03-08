02092b99cc9cf40f0cbc4bf0c7adfff2da270a2f
Mubaraq <adeshinamubarak6@gmail.com>
2026-03-08 23:32:25 +0100

    fix CORS for vercel

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- app.enableCors({
+app.enableCors({
   origin: [
     'http://localhost:3001',
     'https://retail-ai-dashboard.vercel.app',
     /\.vercel\.app$/,
   ],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  credentials: true,
 });
   await app.listen(process.env.PORT || 3000);
 }
